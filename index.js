1601a60ea9388923837fe4e7a95107b5f4d18db8
Alan <89307499+Alan0893@users.noreply.github.com>
2021-08-25 19:39:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-exports.Player    = require('./lib/Player.js')
-exports.Club      = require('./lib/Club.js')
+exports.Client    = require('./src/Client.js')
+exports.Player    = require('./src/Player.js')
+exports.Club      = require('./src/Club.js')
