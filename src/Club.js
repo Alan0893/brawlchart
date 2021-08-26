84f86326099085f935a279914d8967960b814ea8
Alan <89307499+Alan0893@users.noreply.github.com>
2021-08-26 14:49:13 -0400

    Update Club.js

--- src/Club.js
+++ src/Club.js
@@ -9,58 +9,6 @@ class Club {
     this.data     = data.data
   }
   
-  /**
-   * @returns {String} club tag
-   */
-  getTag() {
-    return this.tag
-  }
-
-  /**
-   * @returns {object} trophy progression - daily, weekly, seasonal
-   */
-  getNumber() {
-    return this.number
-  }
-
-  /**
-   * @returns {number} numbers of trophies gained daily
-   */
-  getDaily() {
-    return this.number.daily
-  }
-
-  /**
-   * @returns {number} numbers of trophies gained weekly
-   */
-  getWeekly() {
-    return this.number.weekly
-  }
-  
-  /**
-   * @returns {number} numbers of trophies gained seasonal
-   */
-  getSeasonal() {
-    return this.number.seasonal
-  }
-  
-  /**
-   * @returns {object} data of dates tracked
-   */
-  getLabels() {
-    return this.labels
-  }
-  
-  /**
-   * @returns {object} data of trophies
-   */
-  getData() {
-    return this.data
-  }
-}
-  
-function error(txt) {
-  return TypeError(txt)
 }
 
 module.exports = Club
